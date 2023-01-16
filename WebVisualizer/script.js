3f53691dd699cea4ec7535972dae966020edf1ab
szmanda <michal.szmanda@student.put.poznan.pl>
2023-01-16 15:01:54 +0100

    Visualizer can now send `add` and `remove` requests

--- WebVisualizer/script.js
+++ WebVisualizer/script.js
@@ -1,4 +1,4 @@
-var serverUrl = 'http://127.0.0.1'
+var serverUrl;
 
 // id of the road, number of people waiting, light color
 var setRoadState = function (id, waiting, color) {
@@ -25,18 +25,82 @@ $( document ).ready(function() {
         <div class="widget">
             <div class="light red"></div>
             <div class="counter">0</div>
+            <div class="input-group">
+              <input class="btn btn-secondary btn-add" type="button" value="+">
+              <input class="btn btn-secondary btn-rem" type="button" value="-">
+            </div>
         </div>
     `);
-  waitingRoads.append(widget)
+  waitingRoads.append(widget);
+
+
+  // Setting up the button listeners
+  $('.btn-add').click((e) => {
+    const road = $(e.target).closest('.road, .sidewalk')[0].id;
+    sendCommand('add', road, 1, -10)
+  })
+  $('.btn-rem').click((e) => {
+    const road = $(e.target).closest('.road, .sidewalk')[0].id;
+    sendCommand('remove', road, 1, -10)
+  })
+
+
 
   // setRoadState('in-road-south-1', 3, 'green');
+  
+  // TODO: Move the following to a static class Remote
 
   // TODO: Fetching State every 5 seconds
-  // setInterval(function() {
-  //   console.log("Fetching new state");
-  //   $.ajax({url: serverUrl+'/state', success: function(result){
-  //     console.warn("(not implemented) setting state based on server info");
-  //   }});
-  // }, 5000);
+  /*
+  Expecting json:
+  {
+    "in-road-north-1": {
+      "light" : "red|yellow|green"
+      "waiting-count" : 0
+    },
+    ...
+  }
+  */
+  setInterval(function() {
+    serverUrl = 'http://' + $('#server-url').val() + ':' + $('#server-port').val();
+    console.log(serverUrl);
+    $.ajax({url: serverUrl+'/state', success: function(result){
+      console.warn("(not implemented) setting state based on server info");
+    }})
+      .fail(function(e) { console.log("Error connecting to ", serverUrl) });
+  }, 5000);
 
+
+  // TODO: Send an update
+  /*
+  Changing the number of waiting cars/people in a certain lane
+  - add 'n'
+  - remove 'n'
+  - set to 'n'
+  Sending json:
+  {
+    "in-road-north-1": {
+      "count" : 2
+      "time-offset": -10
+    }
+  }
+  */
+  var sendCommand = function(_command, _road, _count, _timeOffset = 0) {
+    var data = {};
+    data[_road] = {
+      "count" : _count,
+      "time-offset" : _timeOffset
+    }
+    $.ajax({
+      url: serverUrl+'/'+_command,
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json; charset=utf-8',
+      success: function(result){
+        console.warn("(not implemented) sending a json");
+      }
+    }).fail(() => {
+      console.log("Error while sending:", JSON.stringify(data));
+    })
+  }
 });
